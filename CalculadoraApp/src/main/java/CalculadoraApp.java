63232d73c98bd53dce5a180a25de8b936e29f9ec
Fábio <otymus27@gmail.com>
2025-10-24 12:01:27 -0300

    Aula 03 - finalizada

--- CalculadoraApp/src/main/java/CalculadoraApp.java
+++ CalculadoraApp/src/main/java/CalculadoraApp.java
@@ -4,9 +4,23 @@ public class CalculadoraApp {
         return a + b;
     }
 
+    public int divideInteiros(int a, int b) {
+        return a / b;
+    }
+
+    public int multiplicaInteiros(int a, int b) {
+        return a * b;
+    }
+
     public static void main(String[] args) {
         CalculadoraApp app = new CalculadoraApp();
-        int resultado = app.somaInteiros(10, 20);
+        int resultado = app.somaInteiros(10, 5);
         System.out.println(resultado);
+
+        int resultado2 = app.divideInteiros(10, 2);
+        System.out.println(resultado2);
+
+        int resultado3 = app.multiplicaInteiros(10, 3);
+        System.out.println(resultado3);
     }
 }
--- CalculadoraApp/src/test/java/CalculadoraAppTest.java
+++ CalculadoraApp/src/test/java/CalculadoraAppTest.java
@@ -1,13 +1,24 @@
-import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 public class CalculadoraAppTest {
+    CalculadoraApp calc = new CalculadoraApp();
+
+
+    // Antes de cada teste, cria uma nova instancia para o objeto
+    @BeforeEach
+    public void configura(){
+        calc = new  CalculadoraApp();
+    }
+
     @Test
     public void testSomaInteiros(){
-
-        // Configuração do caso de teste
-        CalculadoraApp calc = new CalculadoraApp();
 
         // Execução do caso de teste
         int resultado = calc.somaInteiros(10, 20);
@@ -16,4 +27,31 @@ public class CalculadoraAppTest {
         // Verificação do caso de teste
         assertEquals(30, resultado);
     }
+
+    @Test
+    public void testDivideInteiros(){
+        // Execução do caso de teste
+        int resultado = calc.divideInteiros(10, 5);
+
+
+        // Verificação do caso de teste
+        assertEquals(2, resultado);
+    }
+
+    @Test
+    public void testMultiplicaInteiros(){
+        // Execução do caso de teste
+        int resultado = calc.multiplicaInteiros(10, 5);
+
+
+        // Verificação do caso de teste
+        assertEquals(50, resultado, "Multiplicação mal-sucedida!");
+    }
+
+    // Depois de cada caso de teste, faz limpeza do objeto para liberar espaço em memoria
+    @AfterEach
+    void limpar(){
+        calc = null;
+    }
+
 }
